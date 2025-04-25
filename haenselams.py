08d847cbf042d5b62bfe727160c68b1cdda94eb8
Shareen Arshad <107083486+shareenarshad-io@users.noreply.github.com>
2025-04-25 18:18:42 -0400

    updated code

--- haenselams.py
+++ haenselams.py
@@ -97,7 +97,99 @@ print(result.iloc[:,0:6].sort_values(by = "para1", ascending = False).head(5))
 print(result["para1"].value_counts())
 result = result[result["para1"] < 10]
 
-#Feature Selection
+#Feature Selection & Engineering
 #initialize new array 
 five_best = []
 df_5 = pd.DataFrame(result.corr()["price"]).sort_values(by = "price", ascending = False)
+df_5 = df_5.drop(df_5.index[0]).head(5)
+for i in range(len(df_5)):
+    five_best.append(df_5.index[i]) 
+print(five_best)
+
+#three best
+three_best = []
+df_3 = pd.DataFrame(result.corr()["price"]).sort_values(by = "price", ascending = False)
+df_3 = df_3.drop(df_3.index[0]).head(3)
+for i in range(len(df_3)):
+    three_best.append(df_3.index[i]) 
+
+print(three_best)
+
+#Machine Learning 
+#Regression Models
+feature_sets = {
+    "full_dataset": result.drop(columns=['price']),
+    "three_best": result[three_best],
+    "five_best": result[five_best], 
+   
+}
+
+regression_models = {
+    "Ridge" : linear_model.Ridge(random_state = 42),
+    "DecisionTree" : tree.DecisionTreeRegressor(random_state = 42, max_depth=6),
+    "RandomForest" : RandomForestRegressor(random_state = 42),
+    "XGBoost": XGBRegressor(random_state = 42),
+    "LGBM": LGBMRegressor(random_state = 42),
+    "MLP":  MLPRegressor(random_state = 42),    
+}
+
+def make_regression(x_train, y_train, x_test, y_test, model, model_name, verbose=True):
+
+    model.fit(x_train,y_train)
+    
+    y_predict=model.predict(x_train)
+    train_error = mean_squared_error(y_train, y_predict, squared=False)
+    
+    y_predict =model.predict(x_test)
+    test_error = mean_squared_error(y_test, y_predict, squared=False)
+    
+    y_predict=model.predict(x_train)
+    r2 = r2_score(y_train, y_predict)
+    
+    if verbose:
+        print("----Model name = {}-----".format(model_name))
+        print("Train error = "'{}'.format(train_error))
+        print("Test error = "'{}'.format(test_error))
+        print("r2_score = "'{}'.format(r2))
+        print("--------------------------------")
+    
+    trained_model = model
+    
+    return trained_model, y_predict, train_error, test_error, r2
+
+pred_dict = {
+    "regression_model": [],
+    "feature_set": [],
+    "Train Error": [],
+    "Test Error": [],
+    "R2" : []
+}
+
+for feature_set_name in feature_sets.keys():
+    
+    feature_set = feature_sets[feature_set_name]
+    print("Included columns are {}".format(feature_set_name))
+    for model_name in regression_models.keys():        
+        
+        y = result["price"]
+        x = feature_set
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    
+
+        trained_model, y_predict, train_error, test_error, r2 = make_regression(x_train, y_train, x_test, y_test, regression_models[model_name], model_name, verbose=True)
+
+
+        pred_dict["regression_model"].append(model_name)
+        pred_dict["feature_set"].append(feature_set_name)
+        pred_dict["Train Error"].append(train_error)
+        pred_dict["Test Error"].append(test_error)
+        pred_dict["R2"].append(r2)
+
+
+
+
+#Model Evaluation
+
+#Grid Search in Random Forest
+
+#Deep Learning 
